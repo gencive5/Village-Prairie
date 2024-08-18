7b0298bd49cbc6d98bca5747ae12cc145a76ea8c
gencive5 <vic.segen@gmail.com>
2024-08-18 20:57:53 +0200

    Removed delayed time

--- prairie/src/App.tsx
+++ prairie/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 import VideoFeed from './VideoFeed';
 import { Box } from "@mui/system";
-import DelayedTime from './DelayedTime'; 
+
 import TimeDisplay from './TimeDisplay';
 
 function App() {
@@ -12,9 +12,7 @@ function App() {
 
   return (
     <div>
-    <Box>
-    <DelayedTime />
-  </Box>
+   
     <Box sx={{ position: "fixed", top: 0, left: 0, width: "100vw", height: "100vh", display: "flex", backgroundColor: isNight ? "black" : "cream" }}>
       <Box sx={{ flex: 1, position: "relative" }}>
         <VideoFeed src="http://localhost:8083/stream/pattern/channel/0/hls/live/index.m3u8" />
